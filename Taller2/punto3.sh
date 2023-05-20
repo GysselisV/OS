098efb03afe9c9e6c61b04b60170aac5dd3c6a67
Gysselis Vásquez <110275714+gysselis40@users.noreply.github.com>
2023-05-20 16:36:16 -0500

    Delete Taller2 directory

--- Taller2/punto3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tar -czf folder.tar.gz folder
-
-# Confirmar
-echo "Archivo comprimido:"
-ls -l folder.tar.gz
--- Taller2/punto4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bash punto1.sh
-
-bash punto2.sh
-
-bash punto3.sh
-
